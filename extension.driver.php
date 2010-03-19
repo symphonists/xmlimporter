c3c03a78faf587e1e5544ae8b4a2ce459e244ae8
Brendan Abbott <brendan@bloodbone.ws>
2010-03-19 13:53:40 +1000

    Fix Simulation Problem,  should be modified by prepareImportValue

--- extension.driver.php
+++ extension.driver.php
@@ -1,13 +1,13 @@
 <?php
-	
+
 	require_once(EXTENSIONS . '/xmlimporter/lib/class.xmlimporter.php');
 	require_once(EXTENSIONS . '/xmlimporter/lib/class.xmlimportermanager.php');
-	
+
 	class Extension_XmlImporter extends Extension {
 	/*-------------------------------------------------------------------------
 		Definition:
 	-------------------------------------------------------------------------*/
-		
+
 		public function about() {
 			return array(
 				'name'			=> 'XML Importer',
@@ -19,7 +19,7 @@
 				'description' => 'Import data from XML documents directly into Symphony.'
 			);
 		}
-				
+
 		public function getSubscribedDelegates() {
 			return array(
 				array(
@@ -29,7 +29,7 @@
 				)
 			);
 		}
-		
+
 		public function fetchNavigation() {
 			return array(
 				array(
@@ -39,119 +39,117 @@
 				)
 			);
 		}
-		
+
 		public function initializeAdmin($context) {
 			$page = $context['parent']->Page;
-			
-			//$page->addStylesheetToHead(URL . '/extensions/issuemanager/assets/form.css', 'screen');
 		}
-		
+
 	/*-------------------------------------------------------------------------
 		Utilities:
 	-------------------------------------------------------------------------*/
-		
+
 		public function countXMLImporters() {
 			$xim = new XMLImporterManager($this->_Parent);
 			$results = 0;
-			
+
 			foreach ($xim->listAll() as $about) {
 				$importer = $xim->create($about['handle']);
-				
+
 				if (!$importer->allowEditorToParse()) continue;
-				
+
 				$results++;
 			}
-			
+
 			return $results;
 		}
-		
+
 		public function getXMLImporters($column = 'name', $direction = 'asc', $page = 1, $length = 100000) {
 			$xim = new XMLImporterManager($this->_Parent);
 			$results = array();
-			
+
 			foreach ($xim->listAll() as $about) {
 				$importer = $xim->create($about['handle']);
-				
+
 				if (!$importer->allowEditorToParse()) continue;
-				
+
 				$data = $importer->options();
 				$data['about'] = $about;
 				$results[] = $data;
 			}
-			
+
 			// Sorting:
 			if ($column == 'name') {
 				usort($results, array($this, 'getXMLImportersSortByName'));
 			}
-			
+
 			else if ($column == 'description') {
 				usort($results, array($this, 'getXMLImportersSortByDescription'));
 			}
-			
+
 			else if ($column == 'url') {
 				usort($results, array($this, 'getXMLImportersSortByURL'));
 			}
-			
+
 			else if ($column == 'elements') {
 				usort($results, array($this, 'getXMLImportersSortByElements'));
 			}
-			
+
 			else if ($column == 'modified') {
 				usort($results, array($this, 'getXMLImportersSortByModified'));
 			}
-			
+
 			else if ($column == 'author') {
 				usort($results, array($this, 'getXMLImportersSortByAuthor'));
 			}
-			
+
 			if ($direction != 'asc') {
 				$results = array_reverse($results);
 			}
-			
+
 			// Pagination:
 			$results = array_slice($results, ($page - 1) * $length, $length);
-			
+
 			return $results;
 		}
-		
+
 		protected function getXMLImportersSortByName($a, $b) {
 			return strcmp($a['about']['name'], $b['about']['name']);
 		}
-		
+
 		protected function getXMLImportersSortByDescription($a, $b) {
 			return strcmp($a['about']['description'], $b['about']['description']);
 		}
-		
+
 		protected function getXMLImportersSortByURL($a, $b) {
 			return strcmp($a['source'], $b['source']);
 		}
-		
+
 		protected function getXMLImportersSortByElements($a, $b) {
 			return strcmp($a['included-elements'], $b['included-elements']);
 		}
-		
+
 		protected function getXMLImportersSortByModified($a, $b) {
 			return strtotime($a['about']['updated']) > strtotime($b['about']['updated']);
 		}
-		
+
 		protected function getXMLImportersSortByAuthor($a, $b) {
 			return strcmp($a['about']['author']['name'], $b['about']['author']['name']);
 		}
-		
+
 		public function getXMLImporter($name) {
 			$xim = new XMLImporterManager($this->_Parent);
 			$importer = $xim->create($name);
-			
+
 			$data = $importer->options();
 			$data['about'] = $importer->about();
-			
+
 			return $data;
 		}
-		
+
 		public function setXMLImporter(&$name, &$error, $new) {
 			$template = file_get_contents(EXTENSIONS . '/xmlimporter/templates/xml-importer.php');
 			$old = (!empty($name) ? $this->getXMLImporter($name) : array());
-			
+
 			// Update author:
 			if (!isset($new['about']['author'])) {
 				$new['about']['author'] = array(
@@ -160,23 +158,23 @@
 					'email'		=> $this->_Parent->Author->get('email')
 				);
 			}
-			
+
 			// Update dates:
 			$new['about']['created'] = DateTimeObj::getGMT('c', @strtotime($new['about']['created']));
 			$new['about']['updated'] = DateTimeObj::getGMT('c');
-			
+
 			// New name:
 			$name = str_replace('-', '', Lang::createHandle($new['about']['name']));
-			
+
 			// Create new file:
 			if (strpos(@$new['about']['file'], dirname(__FILE__)) === 0) {
 				$rootdir = dirname(__FILE__);
 			}
-			
+
 			else {
 				$rootdir = WORKSPACE;
 			}
-			
+
 			$filemode = $this->_Parent->Configuration->get('write_mode', 'file');
 			$filename = sprintf(
 				'%s/xml-importers/xml-importer.%s.php',
@@ -184,21 +182,21 @@
 			);
 			$dirmode = $this->_Parent->Configuration->get('write_mode', 'directory');
 			$dirname = dirname($filename);
-			
+
 			// Make sure the directory exists:
 			if (!is_dir($dirname)) {
 				General::realiseDirectory($dirname, $dirmode);
 			}
-			
+
 			// Make sure new file can be written:
 			if (!is_writable($dirname) or (file_exists($filename) and !is_writable($filename))) {
 				$error = __('Cannot save formatter, path is not writable.');
 				return false;
 			}
-			
+
 			$filedata = sprintf(
 				$template,
-				
+
 				// Class name:
 				str_replace(
 					' ', '',
@@ -206,26 +204,26 @@
 						str_replace('-', ' ', Lang::createHandle($new['about']['name']))
 					)
 				),
-				
+
 				// Name:
 				var_export($new['about']['name'], true),
-				
+
 				// Author:
 				var_export($new['about']['author']['name'], true),
-				
+
 				// Website:
 				var_export($new['about']['author']['website'], true),
-				
+
 				// Email:
 				var_export($new['about']['author']['email'], true),
-				
+
 				// Description:
 				var_export($new['about']['description'], true),
-				
+
 				// Dates:
 				var_export($new['about']['created'], true),
 				var_export($new['about']['updated'], true),
-				
+
 				// Options:
 				var_export($new['can-update'], true),
 				var_export($new['fields'], true),
@@ -235,10 +233,10 @@
 				var_export($new['section'], true),
 				var_export($new['unique-field'], true)
 			);
-			
+
 			// Write file to disk:
 			General::writeFile($filename, $filedata, $filemode);
-			
+
 			// Cleanup old file:
 			if (
 				$filename != @$old['about']['html-formatter-file']
@@ -246,19 +244,19 @@
 			) {
 				General::deleteFile($old['about']['html-formatter-file']);
 			}
-			
+
 			return true;
 		}
-		
+
 		public function truncateValue($value) {
 			$max_length = $this->_Parent->Configuration->get('cell_truncation_length', 'symphony');
 			$max_length = ($max_length ? $max_length : 75);
-			
+
 			$value = General::sanitize($value);
 			$value = (strlen($value) <= $max_length ? $value : substr($value, 0, $max_length) . '...');
-			
+
 			return $value;
 		}
 	}
-	
+
 ?>
--- lib/class.xmlimporter.php
+++ lib/class.xmlimporter.php
@@ -1,121 +1,121 @@
 <?php
-	
+
 	require_once(TOOLKIT . '/class.gateway.php');
 	require_once(TOOLKIT . '/class.fieldmanager.php');
 	require_once(TOOLKIT . '/class.entrymanager.php');
 	require_once(TOOLKIT . '/class.sectionmanager.php');
-	
+
 	require_once(EXTENSIONS . '/xmlimporter/lib/class.xmlimporterhelpers.php');
-	
+
 	class XMLImporter {
 		const __OK__ = 100;
 		const __ERROR_PREPARING__ = 200;
 		const __ERROR_VALIDATING__ = 210;
 		const __ERROR_CREATING__ = 220;
-		
+
 		public $_Parent = null;
 		public $_entries = array();
 		public $_errors = array();
-		
+
 		public function __construct($parent) {
 			$this->_Parent = $parent;
 		}
-		
+
 		public function about() {
 			return array();
 		}
-		
+
 		public function options() {
 			return array();
 		}
-		
+
 		public function getEntries() {
 			return $this->_entries;
 		}
-		
+
 		public function getErrors() {
 			return $this->_errors;
 		}
-		
+
 		protected function getExpressionValue($xml, $entry, $xpath, $expression) {
 			$matches = $xpath->evaluate($expression, $entry);
 
 			if ($matches instanceof DOMNodeList) {
 				$values = array();
-				
+
 				foreach ($matches as $match) {
 					if ($match instanceof DOMAttr or $match instanceof DOMText) {
 						$values[] = $match->nodeValue;
 					}
-					
+
 					else {
 						$values[] = $xml->saveXML($match);
 					}
 				}
-				
+
 				return $values;
 			}
-			
+
 			else if (!is_null($matches)) {
 				return array(strval($matches));
 			}
-			
+
 			return null;
 		}
-		
+
 		public function validate($source = null) {
 			if (!function_exists('handleXMLError')) {
 				function handleXMLError($errno, $errstr, $errfile, $errline, $context) {
 					$context['self']->_errors[] = $errstr;
 				}
 			}
-			
+
 			$entryManager = new EntryManager($this->_Parent);
 			$fieldManager = new FieldManager($this->_Parent);
-			
+
 			set_time_limit(900);
 			set_error_handler('handleXMLError');
-			
+
 			$self = $this; // Fucking PHP...
 			$options = $this->options();
-			
+
 			if (!is_null($source)) {
 				$options['source'] = $source;
 			}
-			
+
 			// Fetch document:
 			$gateway = new Gateway();
 			$gateway->init();
 			$gateway->setopt('URL', $options['source']);
 			$gateway->setopt('TIMEOUT', 6);
 			$data = $gateway->exec();
-			
+
 			if (empty($data)) {
 				$this->_errors[] = __('No data to import.');
 				$passed = false;
 			}
-			
+
 			// Load document:
 			$xml = new DOMDocument();
 			$xml->loadXML($data);
-			
+
 			restore_error_handler();
-			
+
 			$xpath = new DOMXPath($xml);
 			$passed = true;
-			
+
 			// Register namespaces:
 			if (is_array($options['namespaces'])) {
 				foreach ($options['namespaces'] as $namespace) {
 					$xpath->registerNamespace($namespace['name'], $namespace['uri']);
 				}
 			}
-			
+
 			// Invalid Markup:
 			if (empty($xml)) {
 				$passed = false;
 			}
-			
+
 			// Invalid Expression:
 			else if (($entries = $xpath->query($options['included-elements'])) === false) {
 				$this->_errors[] = __(
@@ -125,18 +125,18 @@
 				);
 				$passed = false;
 			}
-			
+
 			// No Entries:
 			else if (is_null($entries) or $entries->length == 0) {
 				$this->_errors[] = __('No entries to import.');
 				$passed = false;
 			}
-			
+
 			// Test expressions:
 			else foreach ($options['fields'] as $mapping) {
 				if ($xpath->evaluate(stripslashes($mapping['xpath'])) === false) {
 					$field = $fieldManager->fetch($mapping['field']);
-					
+
 					$this->_errors[] = __(
 						'\'%s\' expression <code>%s</code> is invalid.', array(
 							$field->get('label'),
@@ -146,9 +146,9 @@
 					$passed = false;
 				}
 			}
-			
+
 			if (!$passed) return self::__ERROR_PREPARING__;
-			
+
 			// Gather data:
 			foreach ($entries as $index => $entry) {
 				$this->_entries[$index] = array(
@@ -157,125 +157,128 @@
 					'errors'	=> array(),
 					'entry'		=> null
 				);
-				
+
 				foreach ($options['fields'] as $mapping) {
 					$values = $this->getExpressionValue($xml, $entry, $xpath, $mapping['xpath'], $debug);
-					
+
 					if (isset($mapping['php']) && $mapping['php'] != '') {
 						$php = stripslashes($mapping['php']);
-						
+
 						// static helper
 						if (preg_match('/::/', $php)) {
 							foreach($values as $id => $value) {
 								$values[$id] = call_user_func_array($php, array($value));
 							}
 						}
-						
+
 						// basic function
 						else {
 							foreach($values as $id => $value) {
-								$function = preg_replace('/\$value/', "'" . $value . "'", $php);			
+								$function = preg_replace('/\$value/', "'" . $value . "'", $php);
 								if (!preg_match('/^return/', $function)) $function = 'return ' . $function;
 								if (!preg_match('/;$/', $function)) $function .= ';';
 								$values[$id] = @eval($function);
 							}
 						}
 					}
-					
-					$this->_entries[$index]['values'][$mapping['field']] = $values;					
+
+					$this->_entries[$index]['values'][$mapping['field']] = $values;
 				}
 			}
-			
+
 			// Validate:
 			$passed = true;
-			
+
 			foreach ($this->_entries as &$current) {
 				$entry = $entryManager->create();
 				$entry->set('section_id', $options['section']);
 				$entry->set('author_id', $this->_Parent->Author->get('id'));
 				$entry->set('creation_date', DateTimeObj::get('Y-m-d H:i:s'));
 				$entry->set('creation_date_gmt', DateTimeObj::getGMT('Y-m-d H:i:s'));
-				
+
 				$values = array();
-				
+
 				// Map values:
-				foreach ($current['values'] as $field_id => $value) {					
+				foreach ($current['values'] as $field_id => $value) {
 					$field = $fieldManager->fetch($field_id);
-					
-					// Handle different field types
-					$type = $field->get('type');
-					if($type == 'taglist') {
-						$value = implode(', ', $value);
-					}
-					elseif($type == 'select' || $type == 'selectbox_link' || $type == 'author') {
-						if($field->get('allow_multiple_selection') == 'no') {
-							$value = array(implode('', $value));
-						}
-					}
-					else {
-						$value = implode('', $value);
-					}
-					
+
 					// Adjust value?
 					if (method_exists($field, 'prepareImportValue')) {
 						$value = $field->prepareImportValue($value);
 					}
-					
+
+					else {
+						// Handle different field types
+						// (this should be done by the fields with the above function)
+						$type = $field->get('type');
+						if($type == 'taglist') {
+							$value = implode(', ', $value);
+						}
+						elseif($type == 'select' || $type == 'selectbox_link' || $type == 'author') {
+							if($field->get('allow_multiple_selection') == 'no') {
+								$value = array(implode('', $value));
+							}
+						}
+						else {
+							$value = implode('', $value);
+						}
+					}
+
 					$values[$field->get('element_name')] = $value;
 				}
-				
+
 				// Validate:
 				if (__ENTRY_FIELD_ERROR__ == $entry->checkPostData($values, $current['errors'])) {
 					$passed = false;
 				}
-				
+
 				else if (__ENTRY_OK__ != $entry->setDataFromPost($values, $error, true)) {
 					$passed = false;
 				}
-				
+
 				$current['entry'] = $entry;
 				$current['values'] = $values;
 			}
-			
+
 			if (!$passed) return self::__ERROR_VALIDATING__;
-			
+
 			return self::__OK__;
 		}
-		
+
 		public function commit() {
 			$options = $this->options();
 			$existing = array();
-			
+
 			if ((integer)$options['unique-field'] > 0) {
 				$entryManager = new EntryManager($this->_Parent);
 				$fieldManager = new FieldManager($this->_Parent);
 				$field = $fieldManager->fetch($options['unique-field']);
-				
+
 				if (!empty($field)) foreach ($this->_entries as $index => $current) {
 					$entry = $current['entry'];
-					
+
 					$data = $entry->getData($options['unique-field']);
 					$where = $joins = $group = null;
-					
+
 					$field->buildDSRetrivalSQL($data, $joins, $where);
-					
+
 					$group = $field->requiresSQLGrouping();
 					$entries = $entryManager->fetch(null, $options['section'], 1, null, $where, $joins, false, true);
-					
+
 					if (is_array($entries) && count($entries) > 0) {
 						$existing[$index] = $entries[0]->get('id');
 					}
-					
+
 					else {
 						$existing[$index] = null;
 					}
 				}
 			}
-			
+
 			foreach ($this->_entries as $index => $current) {
 				$entry = $current['entry'];
 				$values = $current['values'];
-				
+
 				// Matches an existing entry
 				if (!empty($existing[$index])) {
 					// update
@@ -283,23 +286,23 @@
 						$entry->set('id', $existing[$index]);
 						$entry->set('importer_status', 'updated');
 					}
-					
+
 					// skip
 					else {
 						$entry->set('importer_status', 'skipped');
 						continue;
 					}
 				}
-				
+
 				// Add data again, without simulation:
-				//$entry->setDataFromPost($values, $error, false);
+				$entry->setDataFromPost($values, $error, false);
 				$entry->commit();
-				
+
 				$status = $entry->get('importer_status');
-				
+
 				if (!$status) $entry->set('importer_status', 'created');
 			}
 		}
 	}
-	
+
 ?>
