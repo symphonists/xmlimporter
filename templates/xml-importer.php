b7e7e475e6de9329de92e3e77760bb5227c7cc92
Nils Hörrmann <post@nilshoerrmann.de>
2014-06-23 14:24:02 +0200

    Format template

--- templates/xml-importer.php
+++ templates/xml-importer.php
@@ -1,37 +1,39 @@
 <?php
 
-	require_once(EXTENSIONS . '/xmlimporter/lib/class.xmlimporter.php');
+require_once(EXTENSIONS . '/xmlimporter/lib/class.xmlimporter.php');
 
-	class XMLImporter%s extends XMLImporter {
-		public function about() {
-			return array(
-				'name'			=> %s,
-				'author'		=> array(
-					'name'			=> %s,
-					'email'			=> %s
-				),
-				'description'	=> %s,
-				'file'			=> __FILE__,
-				'created'		=> %s,
-				'updated'		=> %s
-			);
-		}
+class XMLImporter%s extends XMLImporter {
+    public function about()
+    {
+        return array(
+            'name' => %s,
+            'author' => array(
+                'name' => %s,
+                'email' => %s
+            ),
+            'description' => %s,
+            'file' => __FILE__,
+            'created' => %s,
+            'updated' => %s
+        );
+    }
 
-		public function options() {
-			return array(
-				'can-update'		=> %s,
-				'fields'			=> %s,
-				'included-elements'	=> %s,
-				'namespaces'		=> %s,
-				'source'			=> %s,
-				'timeout'			=> %s,
-				'section'			=> %s,
-				'unique-field'		=> %s
-			);
-		}
+    public function options()
+    {
+        return array(
+            'can-update' => %s,
+            'fields' => %s,
+            'included-elements' => %s,
+            'namespaces' => %s,
+            'source' => %s,
+            'timeout' => %s,
+            'section' => %s,
+            'unique-field' => %s
+        );
+    }
 
-		public function allowEditorToParse() {
-			return true;
-		}
-	}
-
+    public function allowEditorToParse()
+    {
+        return true;
+    }
+}
