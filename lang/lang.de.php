30628c888bbb735a43fb5d51e49e1b2c853fb328
Nils Hörrmann <post@nilshoerrmann.de>
2015-07-22 22:13:46 +0200

    German translation

---
+++ lang/lang.de.php
@@ -0,0 +1,201 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Nils Hörrmann',
+			'email' => 'nils.hoerrmann@hananils.de',
+			'website' => 'http://hananils.de'
+		),
+		'release-date' => '2015-07-22'
+	);
+
+	/**
+	 * XML Importer
+	 */
+	$dictionary = array(
+
+		'XML Importers' =>
+		'XML-Importe',
+
+		'Cannot save formatter, path is not writable.' =>
+		'Formattierer konnte nicht gespeichert werden, weil der Pfad nicht schreibbar ist.',
+
+		'The XMLImporter %s could not be found.' =>
+		'Der XML-Import %s konnte nicht gefunden werden.',
+
+		'Status Report' =>
+		'Statusbericht',
+
+		'Importer' =>
+		'Importe',
+
+		'Created' =>
+		'Erstellt',
+
+		'Updated' =>
+		'Aktualisiert',
+
+		'Skipped' =>
+		'Übersprungen',
+
+		'Failed' =>
+		'Fehlgeschlagen',
+
+		'Action' =>
+		'Aktion',
+
+		'Invalid' =>
+		'Ungültig',
+
+		'Invalid entries' =>
+		'Ungültige Einträge',
+
+		'Import valid entries' =>
+		'Gültige Einträge importieren',
+
+		'Partially complete' =>
+		'Teilweise ausgeführt',
+
+		'Complete' =>
+		'Ausgeführt',
+
+		'Report for %s' =>
+		'Bericht für %s',
+
+		'The import has run successfully without any errors.' =>
+		'Der Import wurde erfolgreich ohne Fehler ausgeführt.',
+
+		'Hide report' =>
+		'Bericht schließen',
+
+		'The import failed due to the following errors:' =>
+		'Der Import schlug aufgrund folgender Fehler fehl:',
+
+		'Entry #%d' =>
+		'Eintrag #%d',
+
+		'Source Data' =>
+		'Ausgangsdaten',
+
+		'Datasource XML' =>
+		'XML aus Datenquelle',
+
+		'Import Data' =>
+		'Importdaten',
+
+		'Symphony Entry' =>
+		'Symphony-Eintrag',
+
+		'Import entry #%d' =>
+		'Eintrag #%d importieren',
+
+		'Name must not be empty.' =>
+		'Der Name darf nicht leer sein.',
+
+		'Source must not be empty.' =>
+		'Die Quelle darf nicht leer sein.',
+
+		'Source is not a valid URL.' =>
+		'Die Quelle ist keine gültige URL.',
+
+		'Failed to retrieve data from source: %s' =>
+		'false',
+
+		'Included Elements must not be empty.' =>
+		'Eingebundene Elemente dürfen nicht leer sein.',
+
+		'An error occurred while processing this form.' =>
+		'Beim Ausführen dieser Anfrage ist ein Fehler aufgetreten.',
+
+		'XML Importer' =>
+		'XML-Import',
+
+		'Run XML Importer' =>
+		'XML-Import ausführen',
+
+		'Description <i>Optional</i>' =>
+		'Beschreibung <i>optional</i>',
+
+		'Choose a DataSource that contains the data you wish to import. Use an XPath expression to select which elements from the source XML to include.' =>
+		'Wählen Sie eine Datenquelle, die Sie importieren möchten. Verwenden Sie einen XPath-Ausdruck um Elemente aus dem Quell-XML einzubinden.',
+
+		'Preview' =>
+		'Vorschau',
+
+		'No DataSource selected.' =>
+		'Keine Datenquelle ausgewählt',
+
+		'Destination' =>
+		'Ziel',
+
+		'Section' =>
+		'Bereich',
+
+		'XPath Expression' =>
+		'XPath-Ausdruck',
+
+		'PHP Function' =>
+		'PHP-Funktion',
+
+		'Is unique' =>
+		'Ist eindeutig',
+
+		'%s No field is unique' =>
+		'%s Kein eindeutiges Feld',
+
+		'If a field is flagged as unique, its value will be used to prevent duplicate entries from being created.' =>
+		'Wenn ein Feld als eindeutig markiert ist, wird sein Wert verwendet, um das Erstellen von Duplikaten zu verhindern.',
+
+		'Can update existing entries' =>
+		'Aktualisieren existierender Eintrag erlauben',
+
+		'Allow entries to be updated from the source, only works when a unique field is chosen.' =>
+		'Einträge können nur dann aktualisiert werden, wenn ein eindeutiges Feld festgelegt wurde.',
+
+		'Create XML Importer' =>
+		'Neuene XML-Import erstellen',
+
+		'Delete this XML Importer' =>
+		'Diesen XML-Import löschen',
+
+		'Modified' =>
+		'Verändert',
+
+		'Create a new XML Importer' =>
+		'Neuen XML-Import erstellen',
+
+		'Sort by {$label} ' =>
+		'Nach {$label} sortieren',
+
+		'None Found.' =>
+		'Keine Ergebnisse',
+
+		'Run' =>
+		'Ausführen',
+
+		'Page %s of %s' =>
+		'Seite %s von %s',
+
+		'Viewing %s - %s of %s entries' =>
+		'Zeige %s – %s von %s Einträgen',
+
+		'No data to import. URL returned HTTP code %d' =>
+		'Keine Daten zum Importieren. URL gab den HTTP-Statuscode %d zurück',
+
+		'No data to import.' =>
+		'Keine Daten zum Importieren.',
+
+		'No field mappings have been set for this XML Importer.' =>
+		'Es wurden keine Feldbezüge für die XML-Import festgelegt.',
+
+		'Root expression <code>%s</code> is invalid.' =>
+		'Der Wurzelausdruck <code>%s</code> ist ungültig.',
+
+		'No entries to import.' =>
+		'Keine Einträge zum Importieren.',
+
+		'\'%s\' expression <code>%s</code> is invalid.' =>
+		'\'%s\' Der Ausdruck <code>%s</code> ist ungültig.',
+
+	);
